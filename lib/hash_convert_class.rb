405828af8df56839a77d5dab8825aa433b9acfa7
e.kulishov <e.kulishov@trucker.group>
2025-04-16 15:39:21 +0300

    Init commit

---
+++ lib/hash_convert_class.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class HashConvertClass
+  def initialize(hash)
+    @hash = hash
+  end
+
+  def self.transform(input_hash); end
+end
---
+++ spec/hash_convert_class_spec.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require_relative '../lib/hash_convert_class'
+
+RSpec.describe HashConvertClass do end
